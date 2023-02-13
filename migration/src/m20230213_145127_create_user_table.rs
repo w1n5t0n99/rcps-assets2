c7bc5a007ec69325bb8dceee6130c8e323a744eb
Reed Elam <relam@SBO-ITLAP-04.rcschools.us>
2023-02-13 15:37:16 -0500

    first commit

---
+++ migration/src/m20230213_145127_create_user_table.rs
@@ -0,0 +1,51 @@
+use sea_orm_migration::prelude::*;
+use chrono::{DateTime, TimeZone, NaiveDateTime, Utc};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(User::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(User::Name).string().not_null())
+                    .col(ColumnDef::new(User::Email).string().not_null().unique_key())
+                    .col(ColumnDef::new(User::Photo).string().not_null().default("default.png"))
+                    .col(ColumnDef::new(User::PasswordHash).string().not_null())
+                    .col(ColumnDef::new(User::CreatedAt).timestamp().not_null())
+                    .col(ColumnDef::new(User::UpdatedAt).timestamp().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum User {
+    Table,
+    Id,
+    Name,
+    Email,
+    Photo,
+    PasswordHash,
+    CreatedAt,
+    UpdatedAt,
+}
