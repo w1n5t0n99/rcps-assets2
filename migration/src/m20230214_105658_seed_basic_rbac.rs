9c18162656068d73a856c825b4e55593e20f12ba
Reed Elam <relam@SBO-ITLAP-04.rcschools.us>
2023-02-14 11:41:28 -0500

    seed rbac data

--- migration/src/m20230214_105658_seed_basic_rbac.rs
+++ migration/src/m20230214_105658_seed_basic_rbac.rs
@@ -1,6 +1,6 @@
 use sea_orm_migration::prelude::*;
 use sea_orm_migration::sea_orm::{entity::*, query::*};
-use ::entity::permissions;
+use ::entity::{permissions, roles, roles_permissions};
 
 
 static PERMS: [&'static str; 8] = ["user_view", "user_edit", "user_create", "apply__item_action", "item_view", "item_edit", "item_create", "edit_settings"];
@@ -14,6 +14,7 @@ impl MigrationTrait for Migration {
         let db = manager.get_connection();
         let transaction = db.begin().await?;
 
+        // insert permissions
         for p in PERMS {
             permissions::ActiveModel {
                 id: Set(p.to_string()),
@@ -21,6 +22,36 @@ impl MigrationTrait for Migration {
             .insert(&transaction)
             .await?;
         }
+
+        // insert roles
+        roles::ActiveModel {
+            id: Set("admin".to_string())
+        }
+        .insert(&transaction)
+        .await?;
+
+        roles::ActiveModel {
+            id: Set("inactive".to_string())
+        }
+        .insert(&transaction)
+        .await?;
+
+        // insert role/permissions
+        for p in PERMS {
+            roles_permissions::ActiveModel {
+                perm_id: Set(p.to_string()),
+                role_id: Set("admin".to_string())
+            }
+            .insert(&transaction)
+            .await?;
+        }
+
+        roles_permissions::ActiveModel {
+            perm_id: Set("user_view".to_string()),
+            role_id: Set("inactive".to_string())
+        }
+        .insert(&transaction)
+        .await?;
 
         transaction.commit().await?;
 
